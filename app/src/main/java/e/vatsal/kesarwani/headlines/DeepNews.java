66736c6301e35c596339faae02c7fe748d0899b1
iamvatsal <vatsalkesarwani12@gmail.com>
2020-02-27 20:17:55 +0530

    improved design

---
+++ app/src/main/java/e/vatsal/kesarwani/headlines/DeepNews.java
@@ -0,0 +1,14 @@
+package e.vatsal.kesarwani.headlines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeepNews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deep_news);
+    }
+}
