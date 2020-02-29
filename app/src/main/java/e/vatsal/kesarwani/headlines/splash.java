e7b1d37efcc7d6e190af14d6a3a397209e53a0cd
iamvatsal <vatsalkesarwani12@gmail.com>
2020-02-29 11:49:08 +0530

    toolbar modified

---
+++ app/src/main/java/e/vatsal/kesarwani/headlines/splash.java
@@ -0,0 +1,17 @@
+package e.vatsal.kesarwani.headlines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
